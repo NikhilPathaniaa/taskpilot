c710969d6c8e102073fbd606d50cdd54328ece30
NikhilPathaniaa <developernikhil1@gmail.com>
2025-03-26 16:50:40 +0530

    color added

--- src/App.jsx
+++ src/App.jsx
@@ -14,23 +14,39 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
       <h1>Add task</h1>
       <form onSubmit={addTask}>
-        <select value={status} onChange={(e) => setStatus(e.target.value)}>
-          <option value="low">Low</option>
+        <select value={status} onChange={(e) => setStatus(e.target.value)} className="border-2 border-blue-300 rounded-xl h-10">
+          <option value="low" className="flex items-center justify-center">
+            Low
+          </option>
           <option value="medium">Medium</option>
           <option value="high">High</option>
         </select>
-        <input type="text" placeholder="Add task" value={task} onChange={(e) => setTask(e.target.value)} />
-        <button type="submit">Add Task</button>
+        <input type="text" className="border-2 border-blue-300 rounded-xl h-10 w-72" placeholder="Add task" value={task} onChange={(e) => setTask(e.target.value)} />
+        <button type="submit" className="bg-gray-300">
+          Add Task
+        </button>
       </form>
 
       <h1>Task List</h1>
       <ul>
         {tasks.map((t, index) => (
           <li key={index}>
-            ({t.status}) {t.text}
+            {t.status == "low" ? (
+              <p className="text-green-500">
+                ({t.status}) {t.text}
+              </p>
+            ) : t.status == "medium" ? (
+              <p className="text-orange-500">
+                ({t.status}) {t.text}
+              </p>
+            ) : (
+              <p className="text-red-500">
+                ({t.status}) {t.text}
+              </p>
+            )}
           </li>
         ))}
       </ul>
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.export = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugin: [],
+};
