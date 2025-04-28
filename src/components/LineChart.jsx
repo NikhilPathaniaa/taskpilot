6a234bf00689a3ca5f6156095b4a930a21d56e68
NikhilPathaniaa <developernikhil1@gmail.com>
2025-04-28 19:07:58 +0530

    idk

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Component mounted! 🚀");
+  }, []);
+
+  useEffect(() => {
+    console.log("Count changed:", count);
+
+    return () => {
+      console.log("Component will UNMOUNT 💀");
+    };
+  }, [count]);
+
+  return (
+    <div>
+      <button className="bg-gray-500 h-10 w-10" onClick={(e) => setCount(count + 1)}>
+        +
+      </button>
+      <div className="text-2xl text-black font-semibold">{count}</div>
+    </div>
+  );
 };
 
 export default LineChart;
